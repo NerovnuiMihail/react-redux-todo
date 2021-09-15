f8da051b3d67a79c1e0a8fe4bfa1020199a57ade
NerovnuiMihail <nerovnuimihail@gmail.com>
2021-09-15 14:53:42 +0300

    delete comments in this component

--- src/pages/Todo/ToDoPage.jsx
+++ src/pages/Todo/ToDoPage.jsx
@@ -82,39 +82,3 @@ const ToDoPage = () => {
 };
 
 export default ToDoPage;
-
-
-
-
-
-
-
-// import React from 'react';
-// import {useSelector} from "react-redux";
-
-// import TodoTable from "./TodoTable";
-// import ToDoUp from "./content/ToDoUP";
-// import ToDoDown from "./content/ToDoDown";
-// import Modal from '../../components/Modal/Modal';
-
-// const ToDoPage = () => {
-    
-//     const visible = useSelector(state => state.modalReducer.visible);
-
-//     return (
-//         <>
-//             {visible && <Modal />}
-        
-//             <div className="todo_page">
-//                 <h1>TODO list</h1>
-//                 <TodoTable />
-//                 <div className="todo_page_content">
-//                     <ToDoUp />
-//                     <ToDoDown />
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default ToDoPage;
